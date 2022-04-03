dbe89d767589c8d035b73f5dace0d09d47142ed4
Brian07 <hoanganh0594abc@gmail.com>
2022-04-03 22:45:26 +0700

    fixbug

--- utils/component.ts
+++
@@ -1,5 +0,0 @@
-import { PATH_HIDDEN_NAVIGATION } from "../constants/path"
-
-export const isHiddenNavigation = (path: string) => {
-    return PATH_HIDDEN_NAVIGATION.some(value => path.includes(value));
-}
